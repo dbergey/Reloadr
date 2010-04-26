3e993dc9b453f8045595da8a02f791fb99d19c4c
Daniel Bergey <dbergey@gmail.com>
2010-04-26 13:58:03 -0400

    Refactored a bit.

--- reloadr.js
+++ reloadr.js
@@ -1,44 +1,81 @@
 /*
-TO USE: include reloadr.js and tell it what to check and how often:
-	Reloadr.watch([
+TO USE: include reloadr.js and tell it what to check:
+
+	Reloadr.go({
+		client: [
+			'/js/main.js',
+			'/css/layout.css'
+		],
+		server: [
+			'index.php'
+		],
+		path: '/reloadr.php',
+		frequency: 2000
+	});
+
+	All keys are optional. If you don't give client or server, though, it won't do anything.
+
+	Shortcut: Reloadr.watch([
 		'/js/main.js',
 		'/css/layout.css'
-	], 2000);
-	
-To watch server-side files, include a path like:
-	'/reloadr.php?*.php,tests/*.php'
-
+	]);
 */
 
 var Reloadr = {
-	frequency: 2000,
+	options: {
+		frequency: 2000,
+		client: [],
+		server: [],
+		path: '/reloadr.php'
+	},
 	req: new XMLHttpRequest(),
-	watchedURLs: [],
-	watch: function(urls, frequency) {
-		for (i in urls)
-			Reloadr.watchedURLs.push(urls[i]);
-		if (frequency) Reloadr.frequency = frequency;
+	timeout: null,
+	watch: function(options) { this.go.call(this, options); },
+	go: function(options) {
+		if ( options )
+
+			// deal with array being passed
+			if ( typeof options.length != 'undefined')
+				this.options.client = options;
+				
+			// change any options given
+			else for (x in options)
+				this.options[x] = options[x];
+
+		// set up new timeout
+		clearTimeout(this.timeout);
+		this.timeout = setTimeout(function() {
+			Reloadr.poll.call(Reloadr);
+		}, this.options.frequency);
 	},
 	ajax: function(url, callback) {
-		Reloadr.req.open("GET", url, false);
-		Reloadr.req.setRequestHeader('If-Modified-Since', window.loadTime.toUTCString());
-		Reloadr.req.send(null);
-		if (Reloadr.req.status == 200)
+		this.req.open("GET", url, false);
+		this.req.setRequestHeader('If-Modified-Since', window._Reloadr_LoadTime.toUTCString());
+		this.req.send(null);
+		if (this.req.status == 200)
 			callback.call(
 				Date.parse(
-					Reloadr.req.getResponseHeader('Last-Modified')
+					this.req.getResponseHeader('Last-Modified')
 				)
 			);
 	},
-	go: function() {
-		window.loadTime = new Date();
-		setInterval(function() {
-			for (i in Reloadr.watchedURLs)
-				Reloadr.ajax(Reloadr.watchedURLs[i], function() {
-					if (this > Date.parse(window.loadTime))
-						location.reload();
-				});
-		}, Reloadr.frequency);
+	poll: function(options) {
+		var urls = this.options.client.slice();
+
+		// build url for server-side files
+		urls.push(this.options.path +'?'+ this.options.server.join(','));
+
+		// check 'em
+		for (i in urls)
+			this.ajax.call(this, urls[i], function() {
+				if (this > Date.parse(window._Reloadr_LoadTime))
+					location.reload();
+			});
+
+		this.go();
+	},
+	init: function(options) {
+		window._Reloadr_LoadTime = new Date();
 	}
 };
-Reloadr.go();
+Reloadr.init();
